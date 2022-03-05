f9c1689fb5b41823374000ae58e1177d394ba7f8
luxuo <emile.hogue.03@gmail.com>
2022-03-05 11:35:17 -0500

    added functionality

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import tictactoe as ttt
 
-piece = ttt.getEmptyBoard()
+piece = ttt.getEmptyBoard() # initialise ttt board
 
+# printBoard(str[9])
+# Prints the tic tac toe game
 def printBoard(piece):
  for i in range(0,9,3):
   print(' '.join(piece[i: i +3]))
 
+# gameOver(str[9], (char, char))
+# returns -> int
+# returns which player has won, tie, or 
+#  if the game hasn't ended yet
 def gameOver(piece, players):
  if ttt.win(players[0], piece):
-  return 0
+  return 0 
  elif ttt.win(players[1], piece):
   return 1 
  elif piece.count(ttt.EMPTY) == 0:
-  return -1
- return 2
+  return -1 # tie
+ return 2 # game isn't over
 
-def move(piece, i, player):
+# move(str[9], int, char)
+# returns -> str[9]
+# Returns the play on the piece
+def move(piece, player, i):
  if piece[i] == ttt.EMPTY:
-  return piece[:i] + player + piece[i+1:]
+  return ttt.playOnPiece(piece,player, i)
  return piece
 
+
+# Game logic begins
 gameState = 2
 player = False
 while gameState == 2:
@@ -29,4 +40,4 @@ while gameState == 2:
  printBoard(piece)
  gameState = gameOver(piece, ttt.PLAYERS)
 
-print(gameState)
+print('Player', gameState, 'has won')
--- tictactoe.py
+++ tictactoe.py
@@ -3,11 +3,24 @@ X = 'X'
 O = 'O'
 PLAYERS = (X, O)
 
+
 # getEmptyBoard()
 # returns -> str[9]
 # Returns an empty tic tac toe board
 def getEmptyBoard():
  return EMPTY * 9
+
+# validGame(str[9], (char, char))
+# returns -> bool
+# Returns wheter the game of tic tac toe
+#  follows the correct playing order
+# PLAYING ORDER : players[0] always
+#  begins the game, then the plays
+#  alternate between players[0] and
+#  players[1]
+def validGame(piece, players):
+ diff = piece.count(players[0]) - piece.count(players[1])
+ return diff == 0 or diff == 1
 
 # win(char, str[9])
 # returns -> bool
@@ -27,6 +40,7 @@ def win(player, piece):
   return True
  return False # No win
 
+
 # permutation(int, str[9])
 # returns -> int
 # Returns the index of the next
@@ -41,13 +55,22 @@ def permutation(index, piece): # TODO RENAME
     return i    
  return -1 # move not possible
  
-# countPossibleWins(str[9], (char, char), set{str[9]}, int = -1)
+
+# playOnPiece(str[9], char, int)
+# returns -> str[9]
+# Returns a new piece with the next play
+def playOnPiece(piece, player, at):
+ return piece[:at] + player + piece[at + 1:]
+
+
+# countPossibleWins(str[9], int, (char, char), set{str[9]}, int = -1)
 # returns -> int
 # Returns the number of possible unique wins that can happen
-#  with the current board
+#  with the current board, assuming the wins are for the next
+#  playing player
 # Assumes that players[0] is the first player to have played
 #  or will play
-def countPossibleWins(piece, players, setBoards = set({}), i = -1):
+def countPossibleWins(piece, player, players, setBoards = set({}), i = -1):
  count = 0 # win counter
  if i == -1: # set i
   i = 9 - piece.count(EMPTY)
@@ -55,16 +78,43 @@ def countPossibleWins(piece, players, setBoards = set({}), i = -1):
   char = players[i % 2] # who plays
 
   split = permutation(play,piece)
-  newPiece = piece[:split] + char + piece[split+1:] # set new board
+  newPiece = playOnPiece(piece, char, split) # set new board
 
   if newPiece not in setBoards:
    setBoards.add(newPiece) # Add in the set
-   if win(players[0], newPiece): # if player[0] wins
+   if win(players[player], newPiece): # if the player wins
     count += 1
-   elif not win(players[1],newPiece): # if player[0] hasn't lost
-    count += countPossibleWins(newPiece, players, setBoards, i + 1)
+   elif not win(players[player],newPiece): # if the player hasn't lost
+    count += countPossibleWins(newPiece, player, players, setBoards, i + 1)
  
  return count
 
- # getWinCounts(str[9], (char, char))
- # 
+
+# getWinCounts(str[9], (char, char))
+# returns -> int[9]
+# Returns an array of all possible win counts
+#  for the next playing player
+def getWinCounts(piece, players):
+ winCounts = [-1] * 9
+
+ availablePlays = piece.count(EMPTY) # how many possible plays
+ if availablePlays == 0: # if the game is over
+  return winCounts
+ 
+ player = (availablePlays - 1) % 2 # determine the 
+ for play in range(availablePlays): # for all available plays
+  index = permutation(play,piece)
+  # get the play
+  newPiece = playOnPiece(piece, players[player], index)
+  # get all possible wins from said play
+  wins = countPossibleWins(newPiece, player, players, set({}))
+  winCounts[index] = wins
+ 
+ return winCounts
+
+# cpuMove(str[9], int, (char, char), int)
+# returns -> str[9]
+# Returns a piece with a cpu making the
+#  next move
+def cpuMove(piece, players, level):
+ pass # TODO
